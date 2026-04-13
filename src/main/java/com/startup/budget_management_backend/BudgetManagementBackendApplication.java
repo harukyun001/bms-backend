435f8399f9cfc53d14bb0fdd0651a2d2e46b658a
harukyun001 <romeo.bantugan.jr@gmail.com>
2026-04-13 21:11:50 +0900

    first commit

---
+++ src/main/java/com/startup/budget_management_backend/BudgetManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.startup.budget_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startup/budget_management_backend/BudgetManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.budget_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
